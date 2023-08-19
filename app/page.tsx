14d71c309f204a3b8512277e8f55bca400cde31b
Shan <lorenzo.cereser@studenti.unitn.it>
2023-08-19 15:43:17 +0200

    mod homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,37 +14,10 @@ export default function Home() {
         />
         <div className="text-center max-w-screen-sm mb-10">
           <h1 className="text-stone-200 font-bold text-2xl">
-            Next.js Prisma PostgreSQL Auth Starter
+            Blockchain project homepage
           </h1>
           <p className="text-stone-400 mt-5">
-            This is a{" "}
-            <a
-              href="https://nextjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Next.js
-            </a>{" "}
-            starter kit that uses{" "}
-            <a
-              href="https://next-auth.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Next-Auth
-            </a>{" "}
-            for simple email + password login and a{" "}
-            <a
-              href="https://vercel.com/postgres"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Vercel Postgres
-            </a>{" "}
-            database to persist the data.
+            This is the homepage of our blockchain project. It is a static page meant to redirect you to the different parts of the app. The whole app is web-based and can be accessed from any device with a browser. It is supported by CI/CD and  deployed on Vercel.
           </p>
         </div>
         <div className="flex space-x-3">
