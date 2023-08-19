09fbc1b43736398190b9e81c8c234407d38b9db9
Shan <lorenzo.cereser@studenti.unitn.it>
2023-08-19 16:10:21 +0200

    mod width and height of img

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col justify-center items-center">
         <Image
-          width={512}
-          height={512}
+          width={900}
+          height={355}
           src="/logo.png"
           alt="Platforms on Vercel"
           className="w-48 h-48"
